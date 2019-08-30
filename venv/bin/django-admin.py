a24fe127d13cff9317d31a886d63edbb80d4e5fb
Muhammed Karakul <muhammedkarakul@gmail.com>
2019-08-31 01:20:29 +0300

    venv created, django and rest api framework installed.

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/muhammedkarakul/Projects/Django/REST_API_Example/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
